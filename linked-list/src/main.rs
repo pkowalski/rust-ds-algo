30cd28a3b4be853a61296bbc8396e89cf551c78f
Patrick Kowalski <patrick.kowalski@finlync.com>
2020-06-02 18:04:37 +0800

    Complete code for linked list

--- linked-list/src/main.rs
+++ linked-list/src/main.rs
@@ -1,37 +1,111 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 
+type Link = Option<Rc<RefCell<Node>>>;
+
+#[derive(Debug, Clone)]
 struct Node {
-    value: i32,
-    next: Option<Box<Node>>
+    value: String,
+    next: Link,
+    prev: Link
 }
 
 struct TransactionLog {
-    head: Option<Box<Node>>,
-    tail: Option<Box<Node>>,
-    pub length: u64
+    head: Link,
+    tail: Link,
+    pub length: u64,
+}
+
+impl Node {
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node { value, next: None, prev: None }))
+    }
+}
+
+pub struct ListIterator {
+    current: Link
+}
+
+impl ListIterator {
+    fn new(start_link: Link) -> ListIterator {
+        ListIterator{
+            current: start_link
+        }
+    }
+}
+
+impl Iterator for ListIterator {
+    type Item = String;
+    fn next(&mut self) -> Option<String> {
+        let current = &self.current;
+        let mut result = None;
+        self.current = match current {
+            Some(ref current) => {
+                let current = current.borrow();
+                result = Some(current.value.clone());
+                current.next.clone()
+            },
+            None => None
+        };
+        result
+    }
+}
+
+impl DoubleEndedIterator for ListIterator {
+    fn next_back(&mut self) -> Option<String> {
+        let mut result = None;
+        let current = &self.current;
+        self.current = match current {
+            Some(ref current) => {
+                let current = current.borrow();
+                let result = Some(current.value.clone());
+                current.prev.clone()
+            },
+            None => None
+        };
+        result
+    }
+}
+
+impl TransactionLog {
+    fn new_empty() -> TransactionLog {
+        TransactionLog {
+            head: None,
+            tail: None,
+            length: 0,
+        }
+    }
+
+    fn append(&mut self, value: String) {
+        let new = Node::new(value);
+
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next = Some(new.clone()),
+            None => self.head = Some(new.clone()),
+        };
+
+        self.length += 1;
+        self.tail = Some(new);
+    }
+
+    fn pop(&mut self) -> Option<String> {
+        self.head.take().map(|head| {
+            if let Some(next) = head.borrow_mut().next.take() {
+                self.head = Some(next);
+            } else {
+                self.tail.take();
+            }
+
+            self.length -= 1;
+            Rc::try_unwrap(head)
+                .ok()
+                .expect("Something is terribly wrong")
+                .into_inner()
+                .value
+        })
+    }
 }
 
 
-fn main() {
-    let n = Node{
-        value: 1,
-        next: None
-    };
-
-    let n_head = Node{
-        value: 2,
-        next: Some(Box::new(n))
-    };
-
-    let t = TransactionLog{
-        head: Some(Box::new(n_head)),
-        tail: Some(Box::new(n)),
-        length: 2
-    };
-
-    let mut head = t.head.expect("No head");
-
-    head.next = Some(Box::new(Node{
-        value: 100,
-        next: None
-    }));
-}
+//TODO add tests
+fn main() {}
